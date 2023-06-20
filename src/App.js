6e4b4387777cec1530fd2ded243861c2e8e3f5a9
woruka onward <worukaonward@gmail.com>
2023-06-20 23:45:09 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+
 
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newStatus, setNewStatus] = useState('');
 
   const addTodo = () => {
     if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
+      const newTodoItem = {
+        title: newTodo,
+        description: newDescription,
+        status: newStatus
+      };
+
+      setTodos([...todos, newTodoItem]);
       setNewTodo('');
+      setNewDescription('');
+      setNewStatus('');
     }
   };
 
@@ -20,21 +33,47 @@ function TodoList() {
   };
 
   return (
-    <div className='my-list'>
+    <div className="todo-list-container">
       <h2>Todo List</h2>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Enter a new task"
-      />
-      <button onClick={addTodo}>Add Task</button>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <input
+          type="text"
+          value={newDescription}
+          onChange={(e) => setNewDescription(e.target.value)}
+          placeholder="Enter a description"
+        />
+        <select
+          value={newStatus}
+          onChange={(e) => setNewStatus(e.target.value)}
+        >
+          <option value="">Select a status</option>
+          <option value="Pending">Pending</option>
+          <option value="Completed">Completed</option>
+        </select>
+        <button onClick={addTodo}>Add Task</button>
+      </div>
 
-      <ul>
+      <ul className="todo-items">
         {todos.map((todo, index) => (
           <li key={index}>
-            {todo}
-            <button onClick={() => removeTodo(index)}>Delete</button>
+            <div className="todo-item">
+              <div>
+                <h3>{todo.title}</h3>
+                <p>{todo.description}</p>
+              </div>
+              <div>
+                <span>Status: {todo.status}</span>
+                <button onClick={() => removeTodo(index)}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
